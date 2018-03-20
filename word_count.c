0c23b6514b9282469497f9fc7b7ab84a47dc6641
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-19 23:06:29 -0400

    Update word_count.c

--- word_count.c
+++ word_count.c
@@ -8,14 +8,14 @@ main()
 {
   int c, nl, nw, nc, state;
   
-  state = OUT
+  state = OUT;
   nl = nw = nc = 0;
   while ((c=getchar()) != EOF) {
     ++nc;
     if (c == '\n')
       ++nl;
     if (c == ' ' || c == '\n' || c == '\t')
-      state = OUT
+      state = OUT;
     else if (state == OUT) {
       state = IN;
       ++nw;
