e2565a3452eccf225c3b8f63650ef64277c4ad02
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-20 12:39:47 -0400

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -5,6 +5,8 @@ main()
 {
   int fahr;
   
+  printf("Temperatures in Fahrenheit and Celsius");
+  
   for (fahr = 0; fahr <= 300; fahr = fahr + 20)
     printf("%3d %6.1f\n",fahr, (5.0/9.0)*(fahr-32));
 }
