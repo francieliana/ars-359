93dbe8bc98d2c1b612c7c7b5865c1b3dbd363818
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-26 19:47:40 -0400

    Create resuable.c

---
+++ resuable.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/* this program can be rewritten for exercises of any kind */
+
+main()
+{
+  int c;
+  if ((c = getchar() != EOF) = 1)
+    printf("uno\n");
+  else
+    print("cero");
+    
+}
