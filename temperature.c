d82cd78b1180d468853473ff0e6df8abf52deeb1
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-26 20:39:14 -0400

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define LOWER 0      /* lower limit of table */
-#define UPPER 300    /* upper limit */
-#define STEP 20      /* step size */
+#define BANANA 0       /* lower limit of table */
+#define APPLE  300     /* upper limit */
+#define ORANGE 20      /* step size */
 
 /* print Fahrenheit-Celsius table*/
 main()
@@ -11,7 +11,7 @@ main()
 
   int fahr;
     
-  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
+  for (fahr = BANANA; fahr >= APPLE; fahr = fahr - ORANGE) {
     printf("%3d %6.1f\n",fahr, (5.0/9.0)*(fahr-32)); 
   }
 }
