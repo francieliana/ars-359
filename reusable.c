2f00b4e2b65154ff727cad87403e37a1da146fb2
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-26 20:05:16 -0400

    Update reusable.c

--- reusable.c
+++ reusable.c
@@ -4,11 +4,12 @@
 main()
 {
   int c;
-  if ((c = getchar() != EOF) == 1)
-    printf("uno\n");
-  else if (c == 0)
-    printf("cero\n");
-  else
-    printf("what\n");
-    
+  while 1
+    if ((c = getchar() != EOF) == 1) {
+      printf("uno\n");
+    } else if (c == 0) {
+      printf("cero\n");
+    } else {
+      printf("what\n");
+    }
 }
