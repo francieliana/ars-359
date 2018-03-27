1b968ba43afe29c96fa3df4b1406b2e7766e8bc9
francieliana <37525260+francieliana@users.noreply.github.com>
2018-03-26 20:37:02 -0400

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0      /* lower limit of table */
+#define UPPER 300    /* upper limit */
+#define STEP 20      /* step size */
+
 /* print Fahrenheit-Celsius table*/
 main()
 {  
-
   printf("Temperatures in Fahrenheit and Celsius\n");
 
   int fahr;
     
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
     printf("%3d %6.1f\n",fahr, (5.0/9.0)*(fahr-32)); 
+  }
 }
